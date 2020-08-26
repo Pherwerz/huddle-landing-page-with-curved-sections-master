33546ae8c1a060cb079e8dca8536bb53ca6dbfa2
pherwerz <lordrisk101@gmail.com>
2020-08-26 13:28:02 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div>fuck it i'm so excited about this</div>;
+  }
+}
+
+export default App;
